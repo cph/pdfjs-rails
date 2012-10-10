84ada033ab676d916ec6ba8d9afaba065a5badc1
Gene Doyel <gene.doyel@cph.org>
2012-10-10 14:58:34 -0500

    Commented out ActionHelper lines

--- lib/pdfjs-rails.rb
+++ lib/pdfjs-rails.rb
@@ -3,11 +3,11 @@ require "pdfjs-rails/version"
 module Pdfjs
   module Rails
     class Engine < ::Rails::Engine
-      paths["app/helpers"] = "lib/helpers"
+      
     end
   end
 end
 
-ActiveSupport.on_load(:action_view) do
-  include Pdfjs::ViewerHelper
-end
+# ActiveSupport.on_load(:action_view) do
+#   include Pdfjs::ViewerHelper
+# end
