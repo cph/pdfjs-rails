8fb130c16b660b9346e4392a0e752e799ce21da8
Robert Lail <robert.lail@cph.org>
2013-02-26 15:12:25 -0600

    [fix] redraw the PDF when changing scale

--- vendor/assets/javascripts/viewer/viewer.js
+++ vendor/assets/javascripts/viewer/viewer.js
@@ -1371,9 +1371,8 @@ window.addEventListener('scalechange', function scalechange(evt) {
       customScaleOption.textContent = Math.round(evt.scale * 10000) / 100 + '%';
       customScaleOption.selected = true;
     }
-    
-    updateViewarea();
   }
+  updateViewarea();
 }, true);
 
 window.addEventListener('pagechange', function pagechange(evt) {
