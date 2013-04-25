cd05be1ce535eb2a58b71fe10d742f3b23e43fbf
Gene Doyel <gene.doyel@cph.org>
2013-04-25 10:02:17 -0500

    [skip] Actually include the console fix

--- vendor/assets/javascripts/pdfviewer.js
+++ vendor/assets/javascripts/pdfviewer.js
@@ -4,6 +4,7 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
+//= require consolefix.js
 //= require compatibility.js
 //= require l10n.js
 //= require pdf.js
